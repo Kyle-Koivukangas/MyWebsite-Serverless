48a543c0938c13ae6da748738c6b3c42f9200987
Kyle-Koivukangas <kyle.koivukangas@gmail.com>
2018-07-04 23:59:35 -0700

    adding rangefinder
    Just a super-quick little component for displaying the current width of the screen,
    so that I can easily see what vuetify width setting for responsive design.

---
+++ mywebsite-vue-app/components/VuetifyRangefinder.vue
@@ -0,0 +1,21 @@
+<template>
+    <!-- Just a little box that shows what the current screen size is. -->
+    <v-container style="position: fixed; top: 50%; left: 50%; background-color: #eee; border: 1.5px solid #999; width: 155px; border-radius: 5px;">
+        <div style="display: inline;">Rangefinder:</div>
+        <v-container style="padding: 10px; display: inline; background-color: #cf0f0f;" xs hidden-sm-and-up>XS</v-container>
+        <v-container style="padding: 10px; display: inline; background-color: #cf4c0f;" sm hidden-xs-only hidden-md-and-up>SM</v-container>
+        <v-container style="padding: 10px; display: inline; background-color: #cf8c0f;" md hidden-sm-and-down hidden-lg-and-up>MD</v-container>
+        <v-container style="padding: 10px; display: inline; background-color: #accf0f;" lg hidden-md-and-down hidden-xl-only>LG</v-container>
+        <v-container style="padding: 10px; display: inline; background-color: #55cf0f;" lg hidden-lg-and-down>XL</v-container>
+    </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
