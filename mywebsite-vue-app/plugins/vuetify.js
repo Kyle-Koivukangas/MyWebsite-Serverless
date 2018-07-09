1e7340bd0286f7ca7443951d25332bb9c6187cc9
Kyle-Koivukangas <kyle.koivukangas@gmail.com>
2018-07-09 02:59:48 -0700

    Added Vuetify Color Scheme and sass files

--- mywebsite-vue-app/plugins/vuetify.js
+++ mywebsite-vue-app/plugins/vuetify.js
@@ -4,13 +4,13 @@ import colors from "vuetify/es5/util/colors";
 
 Vue.use(Vuetify, {
   theme: {
-    primary: "#121212", // a color that is not in the material colors palette
-    accent: colors.grey.darken3,
-    secondary: colors.amber.darken3,
-    info: colors.teal.lighten1,
-    warning: colors.amber.base,
-    error: colors.deepOrange.accent4,
-    success: colors.green.accent3,
+    primary: colors.blue.darken1,
+    secondary: colors.grey.lighten1,
+    accent: colors.lightBlue.lighten2,
+    error: colors.deepOrange.darken2,
+    warning: colors.amber.accent4,
+    info: colors.teal.lighten2,
+    success: colors.green.darken1,
   },
   iconfont: "fa",
   icons: {
